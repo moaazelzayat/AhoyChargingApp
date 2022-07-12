8e6d4036ebb24026c75ea78d2d2e4709b270a088
Moaaz Taha <tmoaaz@gmail.com>
2022-07-12 03:49:28 +0200

    Remove unused code in Resource

--- app/src/main/java/com/ahoy/ahoychargingapp/data/Resource.kt
+++ app/src/main/java/com/ahoy/ahoychargingapp/data/Resource.kt
@@ -41,19 +41,6 @@ fun <T> resourceFlow(initialData: T? = null, resource: suspend () -> T) = flow {
 }.catchErrorsAsResource(initialData)
 
 /**
- * Maps a provided [Flow] to [Resource] values by first emitting a [Resource.Loading] resource.
- * The [catchErrorsAsResource] operator is automatically applied.
- *
- * @param initialData The initial data provided in the loading resource, or in the error resource
- * if an exception is thrown.
- * @see catchErrorsAsResource
- */
-fun <T> Flow<T>.toResourceFlow(initialData: T? = null): Flow<Resource<T>> =
-    map<T, Resource<T>> { Resource.Success(it) }
-        .onStart { emit(Resource.Loading(initialData)) }
-        .catchErrorsAsResource(initialData)
-
-/**
  * Catches any upstream exceptions (except [CancellationException]) and emits them as a
  * [Resource.Error].
  */
@@ -61,23 +48,6 @@ fun <T> Flow<Resource<T>>.catchErrorsAsResource(
     data: T? = null
 ) = catchIgnoreCancellation {
     emit(Resource.Error(data = data, cause = it))
-}
-
-/**
- * For debugging purposes - Convenience function to log each item emitted by this resource flow.
- */
-inline fun <reified T> Flow<Resource<T>>.logEach(
-    tag: String,
-    resourceId: String = T::class.simpleName ?: "Unknown",
-) = onEach {
-    when (it) {
-        is Resource.Error -> Log.e(tag, "~!~ Error [$resourceId] data = ${it.data}", it.cause)
-        is Resource.Loading -> Log.d(tag, "--> Loading [$resourceId] data = ${it.data}")
-        is Resource.Success -> Log.d(tag, "<-- Success [$resourceId] data = ${it.data}")
-    }
-}.onCompletion {
-    val error = it.takeUnless { it is CancellationException }
-    Log.d(tag, "=== Completed [$resourceId] error = $error")
 }
 
 /**
@@ -107,18 +77,4 @@ fun <T> Flow<Resource<T>>.windowedLoadDebounce(
     current
 }.map {
     it.first
-}
-
-/**
- * Returns the first resource that is not [Resource.Loading] as a [Result]
- */
-suspend fun <T> Flow<Resource<T>>.toResult(): Result<T> {
-    val result = catchErrorsAsResource().first {
-        it !is Resource.Loading
-    }
-    return when (result) {
-        is Resource.Loading -> throw IllegalStateException("Received Loading state in toResult")
-        is Resource.Success -> Result.success(result.data)
-        is Resource.Error -> Result.failure(result.cause)
-    }
 }
